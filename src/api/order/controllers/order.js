40db48eba5c4ab15d3fb4884efbf772fd23b5de6
Mohammed Anas <anasmd021@gmail.com>
2026-02-16 16:52:15 +0530

    user specific

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -37,10 +37,10 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   },
 
   async create(ctx) {
-    // Attach the authenticated user to the order
     ctx.request.body.data = {
       ...(ctx.request.body.data || {}),
       orderDate: new Date(),
+      user: ctx.state.user.id, // <-- This line is required!
     };
     return await super.create(ctx);
   },
