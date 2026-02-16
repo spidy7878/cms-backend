fb8bea0dbb864ad82abbe0f309fceb2b6da0afda
Mohammed Anas <anasmd021@gmail.com>
2026-02-16 10:02:03 +0530

    jwt expiry set fr 1hr

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = {};
+/**
+ * @param {object} _env - Strapi environment object (unused)
+ */
+module.exports = (_env) => ({
+  // ...existing config
+  "users-permissions": {
+    config: {
+      jwt: {
+        expiresIn: "1h", // Set JWT expiry to 1 hour
+      },
+    },
+  },
+});
